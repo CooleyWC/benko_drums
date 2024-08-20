4c14939f9aa1328b07570cd294c19cc561c12963
ruth <ruro122020@gmail.com>
2024-08-19 20:47:52 -0400

    playlist in progress

--- src/components/pages/Media.jsx
+++ src/components/pages/Media.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
+import guitar from '../../assets/audio/guitar.mp3'
 
+const songs = [
+  {
+    song: '',
+    artist: '',
+    image: '',
+    audio: ''
+  }
+]
 function Media() {
   return (
-    // <div  >
-    //   {/* Play List */}
-    //   <div>
-    //     <div className='text-green-400'>Play List</div>
-    //     {/* list */}
-    //     <div className=''>
-    //       <img width={50} height={50} src='https://images.unsplash.com/photo-1513277179122-48cc37a9cd96?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTB8fGNhdHN8ZW58MHx8MHx8fDA%3D' />
-    //       <div className=''>
-    //         <span>song name</span>
-    //         <span>artist name</span>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+    <div>
+      {/* Play List */}
+      <div className='container mx-auto sm:px-40 md:px-40 lg:px-52'>{/* mx-auto: centers container. px-4P:adds horizontal padding */}
+        <div className='text-green-400'>Play List</div>
+        {/* list */}
+        <div className='flex border border-gray-400 '>
 
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+          <img className='' width={70} height={70} src='https://images.unsplash.com/photo-1513277179122-48cc37a9cd96?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTB8fGNhdHN8ZW58MHx8MHx8fDA%3D' />
+
+          <div className='flex flex-col pl-1'>
+            <span>Lounge Slow Mo</span>
+            <span>Rizza</span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
