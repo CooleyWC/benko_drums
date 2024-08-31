85cd404d458a5b1559775355ca1e448ba4c85b87
ruth <ruro122020@gmail.com>
2024-08-30 23:55:12 -0400

    audio player functionality complete

--- src/components/pages/media/Media.jsx
+++ src/components/pages/media/Media.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import SongCard from './SongCard'
 import songs from './songs'
 import { TbPlayerSkipForward } from "react-icons/tb";
@@ -6,6 +6,8 @@ import { TbPlayerSkipBack } from "react-icons/tb";
 import { TbPlayerPause } from "react-icons/tb";
 import { TbPlayerPlay } from "react-icons/tb";
 import ambientMusic from '../../../assets/audio/inslowmotion.mp3'
+import guitar from '../../../assets/audio/guitar.mp3'
+
 // import '../media/audio.css'
 
 const calculateTime = (secs) => {
@@ -19,8 +21,9 @@ function Media() {
   const [isPlaying, setIsPlaying] = useState(true)
   const [duration, setDuration] = useState('')
   const [timeCounter, setTimeCounter] = useState(0)
+  //Replace value with first song on the list when it's time to use the real audio
+  const [selectedSong, setSelectedSong] = useState(guitar)
   const audio = useRef()
-
 
   const playPause = () => {
     if (isPlaying) {
@@ -42,13 +45,17 @@ function Media() {
     setDuration(currentTime)
   }
 
+  const handleSelectedSong = (song) => {
+    setSelectedSong(song)
+  }
+
   return (
     <div className='container mx-auto sm:px-40 md:px-40 lg:px-52'>{/* mx-auto: centers container. px-4:adds horizontal padding */}
       <audio
         ref={audio}
-        src={ambientMusic}
+        src={selectedSong}
         preload='metadata'
-        loop
+
         //onLoadedMetadata handles calculating the time duration
         onLoadedMetadata={handleLoadedMetaData}
         //onTimeUpdate updates the time as the song is playing
@@ -75,6 +82,7 @@ function Media() {
             <input
               type="range"
               value={timeCounter}
+              readOnly
             />
           </div>
 
@@ -85,7 +93,7 @@ function Media() {
         {songs.map(songObj => {
           return (
             <div className='pb-1' key={songObj.song}>
-              <SongCard song={songObj} />
+              <SongCard song={songObj} handleSelectedSong={handleSelectedSong} />
             </div>
           )
         })}
