692a42791cd767e73686bda472a86e4424c59174
ruth <ruro122020@gmail.com>
2024-09-01 10:20:52 -0400

    redesigning the playlist

--- src/components/pages/media/Media.jsx
+++ src/components/pages/media/Media.jsx
@@ -3,8 +3,7 @@ import SongCard from './SongCard'
 import songs from './songs'
 import { TbPlayerSkipForward } from "react-icons/tb";
 import { TbPlayerSkipBack } from "react-icons/tb";
-import { TbPlayerPause } from "react-icons/tb";
-import { TbPlayerPlay } from "react-icons/tb";
+
 import guitar from '../../../assets/audio/guitar.mp3'
 
 // import '../media/audio.css'
@@ -17,22 +16,13 @@ const calculateTime = (secs) => {
 }
 
 function Media() {
-  const [isPlaying, setIsPlaying] = useState(true)
   const [duration, setDuration] = useState('')
   const [timeCounter, setTimeCounter] = useState(0)
   //Replace selected song value with first song on the list when it's time to use the real audio
   const [selectedSongInfo, setSelectedSongInfo] = useState(songs[0])
   const audio = useRef()
 
-  const playPause = () => {
-    if (isPlaying) {
-      audio.current.play()
-      setIsPlaying(false)
-    } else {
-      audio.current.pause()
-      setIsPlaying(true)
-    }
-  }
+
   const handleLoadedMetaData = () => {
     setDuration(calculateTime(audio.current.duration))
   }
@@ -49,33 +39,10 @@ function Media() {
     setSelectedSongInfo(song)
   }
 
-  const handleBackSkip = () => {
-    audio.current.currentTime = 0
-  }
 
-  const handleForwardSkip = () => {
-    let index = songs.findIndex(song => song.song === selectedSongInfo.song)
-    if (index === songs[songs.length - 1]) {
-      setSelectedSongInfo(songs[0])
-    } else {
-      setSelectedSongInfo(songs[index + 1])
-    }
 
-    setIsPlaying(true)
-  }
 
-  const handleDoubleBackSkip = () => {
-    let index = songs.findIndex(song => song.song === selectedSongInfo.song)
 
-    if (index === 0) {
-      setSelectedSongInfo(songs[songs.length - 1])
-    } else {
-      setSelectedSongInfo(songs[index - 1])
-    }
-
-    audio.current.currentTime = 0
-    setIsPlaying(true)
-  }
 
   return (
     <div className='container mx-auto sm:px-40 md:px-40 lg:px-52'>{/* mx-auto: centers container. px-4:adds horizontal padding */}
@@ -92,32 +59,8 @@ function Media() {
         onTimeUpdate={updateCurrentTime}
       ></audio>
       <div>
-        {/**Audio Player */}
-        <div className='flex'>
-          {/**Back Skip */}
-          <div>
-            <button onClick={handleBackSkip} onDoubleClick={handleDoubleBackSkip}><TbPlayerSkipBack /></button>
-          </div>
 
-          {/**play & pause */}
-          <div>
-            <button onClick={playPause}> {isPlaying ? <TbPlayerPlay /> : <TbPlayerPause />} </button>
-          </div>
 
-          {/**Forward Skip */}
-          <div>
-            <button onClick={handleForwardSkip}><TbPlayerSkipForward /></button>
-          </div>
-          <div>
-            <input
-              type="range"
-              value={timeCounter}
-              readOnly
-            />
-          </div>
-
-          <div>{duration}</div>
-        </div>
 
 
 
--- src/components/pages/media/SongCard.jsx
+++ src/components/pages/media/SongCard.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-
+import React, { useState, useRef } from 'react'
+import { TbPlayerPause } from "react-icons/tb";
+import { TbPlayerPlay } from "react-icons/tb";
 const SongCard = ({ song, handleSelectedSong }) => {
-  const { song: title, artist, image } = song
+  const { song: title, artist, image, audio: track } = song
+
+  const [isPlaying, setIsPlaying] = useState(true)
+  const audio = useRef()
+
+  const playPause = () => {
+    if (isPlaying) {
+      audio.current.play()
+      setIsPlaying(false)
+    } else {
+      audio.current.pause()
+      setIsPlaying(true)
+    }
+  }
+  const handleLoadedMetaData = () => {
+    setDuration(calculateTime(audio.current.duration))
+  }
+
+  const updateCurrentTime = (e) => {
+    setTimeCounter(Math.floor(audio.current.currentTime))
+    //displays the time next to range input
+    const currentTime = calculateTime(audio.current.duration - Math.floor(e.target.currentTime))
+    setDuration(currentTime)
+  }
   return (
     <div className='flex border border-gray-400' onClick={() => handleSelectedSong(song)}>
+      <audio
+        ref={audio}
+        src={track}
+        preload='metadata'
+
+        //onLoadedMetadata handles calculating the time duration
+        onLoadedMetadata={handleLoadedMetaData}
+        //onTimeUpdate updates the time as the song is playing
+        onTimeUpdate={updateCurrentTime}
+      ></audio>
       <img className='' width={70} height={70} src={image} />
+      <div className='flex'>
+        {/**play & pause */}
+        <div>
+          <button onClick={playPause}> {isPlaying ? <TbPlayerPlay /> : <TbPlayerPause />} </button>
+        </div>
+
+
+      </div>
       <div className='flex flex-col pl-1'>
         <span>{title}</span>
         <span>{artist}</span>
