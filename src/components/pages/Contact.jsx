3c406d6676eca897940091ae6764cb05c1c7e5a2
ruth <ruro122020@gmail.com>
2024-09-08 16:24:44 -0400

    basic form design complete

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -11,16 +11,57 @@ function Contact() {
   }
 
   return (
-    <div>
+    <div className='container w-96'>
       <header>
-        <h1>Contact</h1>
+        <h1 className='border border-black text-center'>Contact</h1>
       </header>
       <main>
         <form onSubmit={handleSubmit}>
-          <input type='text' placeholder='Name' />
-          <input type='text' placeholder='Email' />
-          <textarea rows='4' cols='50' placeholder='Message'></textarea>
-          <input type='submit' />
+          <div className='flex flex-col'>
+            <div className=''>
+              <label
+                for="name"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Name
+              </label>
+              <input
+                id='name'
+                className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                type='text'
+                value={name} onChange={(e) => setName(e.target.value)}
+                required />
+            </div>
+            <div>
+              <label
+                for="name"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Email
+              </label>
+              <input
+                type='text'
+                className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required />
+            </div>
+            <div>
+              <label
+                for="name"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Message
+              </label>
+              <textarea
+                rows='4'
+                cols='50'
+                className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                value={message} onChange={(e) => setMessage(e.target.value)}
+                required></textarea>
+            </div>
+
+            <div>
+              <button type='submit'>Send</button>
+            </div>
+          </div>
         </form>
       </main>
       <footer></footer>
