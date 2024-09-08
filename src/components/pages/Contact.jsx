f4291b90bae66663acd1319da2a099ceea7582a0
ruth <ruro122020@gmail.com>
2024-09-08 14:44:08 -0400

    basic form structure complete

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Contact() {
-    return (
-        <div style={{color: 'white', marginTop: '100px'}}>
-            Contact
-        </div>
-    );
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>Contact</h1>
+      </header>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <input type='text' placeholder='Name' />
+          <input type='text' placeholder='Email' />
+          <textarea rows='4' cols='50' placeholder='Message'></textarea>
+          <input type='submit' />
+        </form>
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default Contact;
