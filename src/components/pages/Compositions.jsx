5c7c58e690a0ea7757677a56255bce4f71fcc548
ruth <ruro122020@gmail.com>
2024-09-25 13:08:16 -0400

    composition in progress

--- src/components/pages/Compositions.jsx
+++ src/components/pages/Compositions.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function Compositions() {
   return (
-    <div className='grid '>
-      <div className='text-center'>
-        <h1>Compositions</h1>
+    <div className="bg-[#D3D3D3]">
+      <div className="">
+        <h1 className="">AT HOME COMPOSITION SERIES</h1>
       </div>
 
       {/**Composition Card */}
-      <div className='border-2 w-1/2'>
-        <div className='flex p-4 justify-around'>
+      <div className="w-1/2 rounded-2xl border-2 border-black">
+        <div className="flex justify-around p-4">
           <div>
             <h5>Title</h5>
             <p>Description</p>
@@ -21,12 +21,23 @@ function Compositions() {
             <button>Buy</button>
           </div>
         </div>
-        <hr className='border-gray-800 w-4/5 content-center m-auto xl:w-3/5' />
 
-        <div className='text-center'>Video</div>
+        <hr className="m-auto w-4/5 content-center border-black xl:w-3/5" />
+
+        <div className="py-4">
+          <iframe
+            className="rounded-2xl"
+            // width="853"
+            // height="480"
+            src="https://www.youtube.com/embed/IQPOZMejwNY?rel=0&wmode=transparent&enablejsapi=1"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen={true}
+            loading="lazy"
+            title="Mug Shot"
+          ></iframe>
+        </div>
       </div>
-
-
     </div>
   );
 }
